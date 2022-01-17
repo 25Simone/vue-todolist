5d4299df2311550f61e7b8747c68bd5876f79050
25Simone <dantsimone111@gmail.com>
2022-01-17 14:51:15 +0100

    Milestone 1 completata

---
+++ js/script.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app',
+    data: {
+        todo: [
+            {
+                text: 'Fare i compiti',
+                done: false,
+            },
+
+            {
+                text: 'Fare la spesa',
+                done: true,
+            },
+
+            {
+                text: 'Fare il bucato',
+                done: false,
+            },
+
+            {
+                text: "Pagare l'affitto",
+                done: true,
+            },
+        ],
+    },
+});
